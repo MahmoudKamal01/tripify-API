073d349b32f3588cd2ca9d1bdfac99e5ea449f08
MahmoudKamal01 <mahmoudkamalmk01@gmail.com>
2024-08-06 19:16:47 +0300

    changed some connection setting in mongo

--- utils/connectToDb.js
+++ utils/connectToDb.js
@@ -5,7 +5,12 @@ const { MONGO_URI } = require("../configs/keys.js");
 const connectToMongoDB = async () => {
   try {
     mongoose
-      .connect(MONGO_URI)
+      .connect(MONGO_URI, {
+        maxIdleTimeMS: 80000,
+        serverSelectionTimeoutMS: 80000,
+        socketTimeoutMS: 0,
+        connectTimeoutMS: 0,
+      })
       .then(() => console.log("MongoDB connected"))
       .catch((err) => console.error("MongoDB connection error:", err));
   } catch (error) {
